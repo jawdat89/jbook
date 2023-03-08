c949ec90289b393584c2e999be6f62f8b37597f5
Jawdat Abadullah <Jawdat.89@outlook.com>
2023-03-08 21:42:11 +0200

    caching with key-value pairs

--- src/plugins/unpkg-path-plugin.tsx
+++ src/plugins/unpkg-path-plugin.tsx
@@ -1,6 +1,11 @@
 import * as esbuild from 'esbuild-wasm';
 
 import axios from 'axios';
+import localforage from 'localforage';
+
+const fileCache = localforage.createInstance({
+  name: 'filecache'
+});
      
 export const unpkgPathPlugin = () => {
   return {
@@ -37,15 +42,27 @@ export const unpkgPathPlugin = () => {
             `,
           };
         } 
+
+        // Check to see if we have already fetched this file
+        //and if it is in the cache
+        const cachedResult = await fileCache.getItem(args.path);
+        // if it is in, return it immediatly
+        if (cachedResult) {
+          return cachedResult;
+        }
         
         const { data, request } = await axios.get(args.path)
 
-        return {
+        const result = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname
         }
 
+        // store response in cache
+        await fileCache.setItem(args.path, result);
+
+        return result;
       });
     },
   };
