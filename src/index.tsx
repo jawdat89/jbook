9247ad123d9417c4d62848208ad6dea12fbdb025
Jawdat Abadullah <Jawdat.89@outlook.com>
2023-04-02 22:51:57 +0300

    Refactoring Out a Preview Component

--- src/index.tsx
+++ src/index.tsx
@@ -5,32 +5,13 @@ import ReactDom from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './components/code-editor';
+import Preview from './components/preview';
 
 const App = () => {
 
   const ref = useRef<any>();
-  const iframe = useRef<any>();
+  const [code, setCode] = useState('');
   const [input, setInput] = useState('');
-
-  const html = `
-    <html>
-      <head></head>
-      <body>
-        <div id="root"></div>
-        <script>
-          window.addEventListener('message', (event) => {
-            try {
-              eval(event.data);
-            } catch (err) {
-              const root = document.querySelector('#root');
-              root.innerHTML = '<div style="color:red;"><h4>Runtime Error</h4>' + err + '</div>';
-              console.error(err);
-            }
-          }, false);
-        </script>
-      </body>
-    </html>
-  `;
 
   useEffect(() => { 
     startService();
@@ -48,8 +29,6 @@ const App = () => {
       return;
     }
 
-    iframe.current.srcdoc = html;
-
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -64,8 +43,7 @@ const App = () => {
       }
     });
 
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
-
+    setCode(result.outputFiles[0].text);
   };
 
   return (
@@ -74,11 +52,10 @@ const App = () => {
         initialValue='const a = 1;'
         onChange={(value) => setInput(value)}
       />
-      <textarea onChange={(e) => setInput(e.target.value)} />
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <iframe title="preview" ref={iframe} sandbox='allow-scripts' srcDoc={html} />
+      <Preview code={code}  />
     </div>
   );
 };
